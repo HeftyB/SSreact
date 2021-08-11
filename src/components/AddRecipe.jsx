46a7adafb4eccd594fb3946ef39891d720a13353
HeftyB <Ashields0923@gmail.com>
2021-08-10 23:34:51 -0400

    add recipe form update

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -40,13 +40,12 @@ export default function AddRecipe (props) {
     };
 
     const ingredientAdder = e => {
-      setRecipe({ingredients: ["test bitch"]});
-      // setRecipe({
-      //   ...recipe,
-      //   ingredients: x
-      // });
+      setRecipe({
+        ...recipe,
+        ingredients: [...recipe.ingredients, ingredient]
+      });
 
-      // setIngredient(initialIngredient);
+      setIngredient(initialIngredient);
     };
 
     const stepHandler = e => {
@@ -85,6 +84,30 @@ export default function AddRecipe (props) {
       });
     };
 
+    const stepOrder = (index, dir) => {
+      if (index + dir < 0 || index + dir > recipe.steps.length - 1) {
+        // consol
+        console.log("catch")
+        return;
+      }
+
+      let x = [...recipe.steps];
+      let a = x[index]; 
+      let b = x[index + dir];
+
+      console.log(index)
+      console.log(dir)
+      console.log(index + dir)
+
+      x[index + dir] = b;
+      x[index] = a;
+      
+      setRecipe({
+        ...recipe,
+        steps: x
+      })
+    };
+
     return (
         <div className="bg-color4 w-10/12 mx-auto h-full">
           <div className="text-3xl text-center font-bold">Add Recipe</div>
@@ -104,61 +127,66 @@ export default function AddRecipe (props) {
                     <input type="text" id="url" name="url" value={recipe.url} onChange={handler} className="w-3/5"/>
                 </div>
 
-                <div className="border border-blue-800 w-9/12 mx-auto my-4">
-                  <div className="border border-black">
-                    <div className="text-center text-xl">Ingredients:</div>
-                    <table className="w-full">
-                      <tbody>
-                        <tr>
-                          <th>QTY</th>
-                          <th>Ingredient</th>
-                          <th>Delete</th>
-                        </tr>
-                      </tbody>
-                      {/* {recipe.ingredients.map((i, index) => {
-                      return(
-                        <tbody>
-                          <tr key={index}>
-                            <td>{i.qty}</td>
-                            <td>{i.ingredient}</td>
-                            <td onClick={delIngredient(index)}>X</td>
-                          </tr>
-                        </tbody>
-                      )
-                    })} */}
-                    </table>
-                  </div>
-                  <div className="flex justify-around p-4">
-                      <label htmlFor="qty">qty: </label>
-                      <input type="text" id="qty" name="qty" value={ingredient.qty} onChange={ingredientHandler}/>
-                  </div>
-                  <div className="flex justify-around p-4">
-                      <label htmlFor="ingredient">Ingredient: </label>
-                      <input type="text" id="ingredient" name="ingredient" value={ingredient.ingredient} onChange={ingredientHandler}/>
-                  </div>
-                  <div className="text-center mx-auto border border-dashed border-black bg-red-600 text-xl" onClick={ingredientAdder}>Add Ingredient</div>
-                </div>
+                <div className="w-9/12 mx-auto my-4">
+                  <div className="">
+                    <div className="text-center text-3xl font-bold">Ingredients:</div>
+                    <hr className="border-black"/>
+                    <div className="">   
+                      <div className="flex text-center text-xl font-bold">
+                        <div className="w-1/5 border-black boarder-r">QTY</div>
+                        <div className="w-3/5 border-black border-l border-r">INGREDIENT</div>
+                        <div className="w-1/5 border-black boarder-l">DEL</div>
+                      </div>
 
-                <div className="border border-blue-800 w-9/12 mx-auto my-4">
-                  <div className="border border-black">
-                    <div className="text-center text-xl">Steps:</div>
-                    <ul>
-                    {recipe.steps.map(s => {
-                      return(
-                        <li>{`${s.stepNum}: ${s.step}`}</li>
-                      )
-                    })}
-                    </ul>
+                      {recipe.ingredients.map((i, index) => {
+                        return (
+                          <div className="flex text-center border-t border-b border-black py-2" key={index}>
+                            <div className="w-1/5">{i.qty}</div>
+                            <div className="w-3/5 border-black border-l border-r">{i.ingredient}</div>
+                            <div className="w-1/5 font-extrabold text-red-500" onClick={e => delIngredient(index)}>X</div>
+                          </div>
+                        )
+                      })}
+
+                    </div>
                   </div>
-                  <div className="flex justify-around p-4">
-                      <label htmlFor="stepNum">Step Number: </label>
-                      <input type="text" id="stepNum" name="stepNum" value={step.stepNum} onChange={stepHandler}/>
+                  <div className="border border-black my-2">
+                    <input className="w-1/5 text-center border-black border-r" type="text" id="qty" name="qty" placeholder="QTY" value={ingredient.qty} onChange={ingredientHandler}/>
+                    <input className="w-4/5 text-center" type="text" id="ingredient" name="ingredient" placeholder="INGREDIENT" value={ingredient.ingredient} onChange={ingredientHandler}/>
                   </div>
-                  <div className="flex justify-around p-4">
-                      <label htmlFor="step">Step Instruction: </label>
-                      <input type="text" id="step" name="step" value={step.step} onChange={stepHandler}/>
+                  <div className="text-center mx-auto bg-red-600 text-xl" onClick={ingredientAdder}>Add Ingredient</div>
+                </div>
+                <hr />
+                <hr />
+                <div className="w-9/12 mx-auto my-4">
+                  <div className="">
+                    <div className="text-center text-3xl font-bold">Steps:</div>
+                    <hr className="border-black"/>
+                    <div className="">   
+                      <div className="flex text-center text-xl font-bold">
+                        <div className="w-full border-black border-r">STEP</div>
+                        <div className="w-20 border-black boarder-l text-lg">ORDER</div>
+                      </div>
+                      
+                      {recipe.steps.map((s, index) => {
+                        return (
+                          <div className="flex text-center border-t border-b border-black py-2" key={index}>
+                            <div className="w-full text-left border-black border-r px-2">{s.step}</div>
+                            <div className="w-20 flex justify-evenly">
+                              <span className={index == 0 ? "my-auto font-bold cursor-not-allowed" : "my-auto font-bold cursor-pointer"} onClick={e => stepOrder(index, -1)}>↑</span>
+                              <span className={index == recipe.steps.length - 1 ? "my-auto font-bold cursor-not-allowed" : "my-auto font-bold cursor-pointer"} onClick={e => stepOrder(index, 1)}>↓</span>
+                              <span className="my-auto font-extrabold text-red-500 cursor-pointer" onClick={e => delStep(index)}>X</span>
+                            </div>
+                          </div>
+                        )
+                      })}
+
+                    </div>
                   </div>
-                  <div className="text-center mx-auto border border-dashed border-black bg-red-600 text-xl" onClick={stepAdder}>Submit</div>
+                  <form className="border border-black my-2">
+                    <textarea className="text-center w-full" id="step" name="step" placeholder="STEP INSTRUCTION" value={step.step} onChange={stepHandler}/>
+                  </form>
+                  <div className="text-center mx-auto bg-red-600 text-xl" onClick={stepAdder}>Add Step</div>
                 </div>
                 <div className="bg-green-400 border-black border-dotted text-xl text-center font-bold">SUBMIT RECIPE</div>
             </div>
