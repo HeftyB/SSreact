e08d32a6fe7128d671ee76c3e8cf559cc7458b44
HeftyB <Ashields0923@gmail.com>
2021-08-21 23:46:11 -0400

    animation update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blowUpModal: {
+          '0%': {transform: 'scale(0)', opacity: '0'},
+          '100%': {transform: 'scale(1)', opacity: '1'}
+        },
+        blowUpModalTwo: {
+          '0%': {transform: 'scale(1)', opacity: '1'},
+          '100%': {transform: 'scale(0)', opacity: '0'}
+        },
+      },
+      animation: {
+        blowUpModal: "blowUpModal 2s cubic-bezier(0.165, 0.84, 0.44, 1) forwards",
+        blowUpModalTwo: "blowUpModalTwo 2s cubic-bezier(0.165, 0.84, 0.44, 1) forwards",
+      }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
